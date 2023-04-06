7d22789002ccc6a6ccc9d7d7bfa30629d36bf8db
Ujjwal Kumar <ujjwalsinghrajput2000@gmail.com>
2023-04-07 02:19:04 +0530

    hii

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import gpt3Logo from '../../logo.svg';
-import './footer.css';
+// eslint-disable-next-line quotes
+import React from "react";
+// eslint-disable-next-line quotes
+import gpt3Logo from "../../logo.svg";
+// eslint-disable-next-line quotes
+import "./footer.css";
 
 const Footer = () => (
   <div className="gpt3__footer section__padding">
     <div className="gpt3__footer-heading">
-      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      <h1 className="gradient__text">
+        Do you want to step in to the future before others
+      </h1>
     </div>
 
     <div className="gpt3__footer-btn">
@@ -15,7 +20,9 @@ const Footer = () => (
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
-        <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+        <p>
+          ujjwalsinghrajput2000@gmail.com, <br /> All Rights Reserved
+        </p>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Links</h4>
@@ -32,7 +39,7 @@ const Footer = () => (
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
+        <p>ujjwalsinghrajput2000@gmail.com</p>
         <p>085-132567</p>
         <p>info@payme.net</p>
       </div>
